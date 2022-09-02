392c829ecdc8924e54b538f36e63431aeea868cc
alex6dj <alexeiagueroalba@gmail.com>
2022-09-01 22:11:40 -0400

    Drop use of Xml in FileSettingsStore

--- src/MultiConverter.Services.Abstractions/Settings/State.cs
+++ src/MultiConverter.Services.Abstractions/Settings/State.cs
@@ -1,9 +1,12 @@
-﻿namespace MultiConverter.Services.Abstractions.Settings;
+﻿using System.Text.Json.Serialization;
 
-public readonly record struct State(int Version = 0, string Value = "")
+namespace MultiConverter.Services.Abstractions.Settings;
+
+public readonly record struct State(int Version = 0, [property: JsonConverter(typeof(RawJsonConverter))]
+    string Value = "")
 {
-    public static readonly State Empty = new State(0, string.Empty);
-    
+    public static readonly State Empty = new(0, string.Empty);
+
     public override string ToString()
     {
         return $"Version: {Version}, Value: {Value}";
--- src/MultiConverter.Services.Settings/FileSettingsStore.cs
+++ src/MultiConverter.Services.Settings/FileSettingsStore.cs
@@ -1,6 +1,6 @@
 using System;
 using System.IO;
-using System.Xml.Linq;
+using System.Text.Json;
 using Microsoft.Extensions.Logging;
 using MultiConverter.Services.Abstractions.Settings;
 
@@ -18,7 +18,10 @@ public sealed class FileSettingsStore : ISettingsStore
             "MultiConverter");
 
         var dir = new DirectoryInfo(Location);
-        if (!dir.Exists) dir.Create();
+        if (!dir.Exists)
+        {
+            dir.Create();
+        }
 
         _logger.LogInformation("Settings folder is {Location}", Location);
     }
@@ -32,16 +35,17 @@ public sealed class FileSettingsStore : ISettingsStore
         var file = Path.Combine(Location, $"{key}.setting");
         var info = new FileInfo(file);
 
-        if (!info.Exists || info.Length == 0) return State.Empty;
+        if (!info.Exists || info.Length == 0)
+        {
+            return State.Empty;
+        }
 
-        var doc = XDocument.Load(file);
-        var root = doc.ElementOrThrow(Structure.root);
-        var versionString = root.AttributeOrThrow(Structure.version);
-        var version = int.Parse(versionString);
-        var state = root.ElementOrThrow(Structure.state);
+        var value = File.ReadAllText(file);
+
+        var state = JsonSerializer.Deserialize<State>(value);
 
         _logger.LogDebug("{Key} has the value {State}", key, state);
-        return new State(version, state);
+        return state;
     }
 
     public void Save(string key, State state)
@@ -50,21 +54,10 @@ public sealed class FileSettingsStore : ISettingsStore
 
         _logger.LogInformation("Creating setting for {Key}", key);
 
-        var root = new XElement(new XElement(Structure.root, new XAttribute(Structure.version, state.Version)));
-        root.Add(new XElement(Structure.state, state.Value));
-
-        var doc = new XDocument(root);
-        var fileText = doc.ToString();
+        var fileText = JsonSerializer.Serialize(state);
 
         _logger.LogInformation("Writing settings for {Key} to {File}", key, file);
         File.WriteAllText(file, fileText);
         _logger.LogInformation("Setting  for {Key} committed", key);
-    }
-
-    private static class Structure
-    {
-        internal static readonly string root = "Setting";
-        internal static readonly string state = "State";
-        internal static readonly string version = "Version";
     }
 }
