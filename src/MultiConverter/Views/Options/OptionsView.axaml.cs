31e57720268c30049c83701d8ed0c53cb4384a67
alex6dj <alexeiagueroalba@gmail.com>
2022-10-04 17:36:03 -0400

    Initial OptionsView implementation

---
+++ src/MultiConverter/Views/Options/OptionsView.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using MultiConverter.ViewModels.Options;
+using ReactiveUI;
+
+namespace MultiConverter.Views.Options;
+
+public partial class OptionsView : ReactiveUserControl<OptionsViewModel>
+{
+    public OptionsView()
+    {
+        InitializeComponent();
+        this.WhenActivated(_ => { });
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
