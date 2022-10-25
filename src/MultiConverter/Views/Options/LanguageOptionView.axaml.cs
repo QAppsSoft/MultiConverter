6e03456e3772b984082b3b5972679f604f26ae15
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2022-10-25 03:50:16 -0400

    Initial IOptionItem views implementation

---
+++ src/MultiConverter/Views/Options/LanguageOptionView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MultiConverter.Views.Options;
+
+public partial class LanguageOptionView : UserControl
+{
+    public LanguageOptionView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
---
+++ src/MultiConverter/Views/Options/TemporalPathOptionView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MultiConverter.Views.Options;
+
+public partial class TemporalPathOptionView : UserControl
+{
+    public TemporalPathOptionView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
