c179ae3d8b201246f387f4ff818060e5e26f27de
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2022-10-28 10:46:02 -0400

    Update usage of immediate scheduler

--- tests/MultiConverter.Services.ScannerFixtures/PathScannerTests.cs
+++ tests/MultiConverter.Services.ScannerFixtures/PathScannerTests.cs
@@ -10,24 +10,16 @@ namespace MultiConverter.Services.ScannerFixtures;
 
 public class PathScannerTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Enumerate_Directory()
     {
         int counter = 0;
 
-        TestSchedulers schedulerProvider = new();
+        ImmediateSchedulers schedulerProvider = new();
         string systemDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         PathScanner pathScanner = new(systemDirectory, schedulerProvider);
 
         using IDisposable disposable = pathScanner.Files.Take(10).Count().Subscribe(value => counter = value);
-
-        schedulerProvider.CurrentThread.AdvanceBy(TimeSpan.FromMinutes(1).Ticks);
-        schedulerProvider.CurrentThread.Start();
 
         counter.Should().BeGreaterThan(0);
     }
@@ -38,13 +30,10 @@ public class PathScannerTests
         int counter = 0;
         using TemporalFile temporalFile = TemporalFile.Create("EnumerateSingleFile.txt");
 
-        TestSchedulers schedulerProvider = new();
+        ImmediateSchedulers schedulerProvider = new();
         PathScanner pathScanner = new(temporalFile, schedulerProvider);
 
         using IDisposable disposable = pathScanner.Files.Count().Subscribe(value => counter = value);
-
-        schedulerProvider.CurrentThread.AdvanceBy(TimeSpan.FromMinutes(1).Ticks);
-        schedulerProvider.CurrentThread.Start();
 
         counter.Should().Be(1);
     }
