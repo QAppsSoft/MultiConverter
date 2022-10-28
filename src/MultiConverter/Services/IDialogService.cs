858b14ed1fd1651ae281b63dd31bd003696dfdaa
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2022-10-28 06:58:46 -0400

    Use internal IDialogService implementation

---
+++ src/MultiConverter/Services/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace MultiConverter.Services;
+
+public interface IDialogService
+{
+    Task<string?> ShowFolderSelectorAsync(FolderDialogSettings? options = null);
+}
