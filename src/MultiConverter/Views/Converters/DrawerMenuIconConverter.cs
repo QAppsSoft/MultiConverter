c59b0bc281bed4d19a9545b1a43276ec20dcdacd
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-01-24 14:06:17 -0500

    Update Material.Avalonia to latest preview version and done some minor fixes

---
+++ src/MultiConverter/Views/Converters/DrawerMenuIconConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Material.Icons;
+
+namespace MultiConverter.Views.Converters;
+
+public class DrawerMenuIconConverter : ConvertBase
+{
+    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool isOpened)
+        {
+            return isOpened ? MaterialIconKind.ArrowLeft : MaterialIconKind.Menu;
+        }
+
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+}
