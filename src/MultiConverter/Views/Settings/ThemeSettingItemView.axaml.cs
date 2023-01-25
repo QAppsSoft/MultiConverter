9664cac47c4137604bf68e5b26df1c04ffaa6980
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-01-25 09:35:31 -0500

    [WIP] Initial ThemeSettingItemView implementation

---
+++ src/MultiConverter/Views/Settings/ThemeSettingItemView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MultiConverter.Views.Settings;
+
+public partial class ThemeSettingItemView : UserControl
+{
+    public ThemeSettingItemView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
