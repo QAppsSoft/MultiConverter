c43b40af4d52d73e593d64c5e42f863035b79b76
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-01-25 22:22:10 -0500

    Implement supported file extensions

---
+++ src/MultiConverter/Views/Converters/SymbolConverters.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Data.Converters;
+using Material.Icons;
+
+namespace MultiConverter.Views.Converters;
+
+public static class SymbolConverters
+{
+    /// <summary>
+    ///     A value converter that returns an icon if the input bool is true or false, representing an Edit state
+    /// </summary>
+    public static readonly IValueConverter EditStatusToSymbol =
+        new FuncValueConverter<bool, MaterialIconKind>(value =>
+            value ? MaterialIconKind.ContentSave : MaterialIconKind.Edit);
+}
+
