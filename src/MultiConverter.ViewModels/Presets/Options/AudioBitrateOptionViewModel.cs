4cdbd2f2794b816a1b4d18f16154e316203b7eec
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-03-06 15:14:27 -0500

    Implement AudioBitrateOptionViewModel and testing

---
+++ src/MultiConverter.ViewModels/Presets/Options/AudioBitrateOptionViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Reactive.Linq;
+using MultiConverter.Common;
+using MultiConverter.Models.Presets.Interfaces;
+using MultiConverter.Models.Presets.Options;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace MultiConverter.ViewModels.Presets.Options;
+
+public class AudioBitrateOptionViewModel : OptionViewModelBase
+{
+    public AudioBitrateOptionViewModel(AudioBitrateOption audioBitrateOption, ISchedulerProvider schedulerProvider) :
+        base(schedulerProvider)
+    {
+        Bitrate = audioBitrateOption.Bitrate;
+
+        _ = this.WhenAnyValue(x => x.Bitrate)
+            .Select(bitrate => bitrate != audioBitrateOption.Bitrate)
+            .ObserveOn(schedulerProvider.Dispatcher)
+            .ToPropertyEx(this, x => x.HasChanged);
+
+        InitializeDefaultOptions();
+    }
+
+    [Reactive] public int Bitrate { get; set; }
+
+    private void InitializeDefaultOptions() =>
+        DefaultOptions = new[]
+        {
+            new ValuesUpdater { Caption = "128 kbps", Update = () => Bitrate = 128 },
+            new ValuesUpdater { Caption = "196 kbps", Update = () => Bitrate = 196 },
+            new ValuesUpdater { Caption = "256 kbps", Update = () => Bitrate = 256 },
+            new ValuesUpdater { Caption = "320 kbps", Update = () => Bitrate = 320 }
+        };
+
+    public static implicit operator AudioBitrateOption(AudioBitrateOptionViewModel vm) => new(vm.Bitrate);
+
+    public static IOption ToOption(AudioBitrateOptionViewModel vm) => new AudioBitrateOption(vm.Bitrate);
+}
---
+++ tests/MultiConverter.ViewModelsFixtures/Presets/AudioBitrateOptionViewModelTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using MultiConverter.Common;
+using MultiConverter.Common.Testing;
+using MultiConverter.Models.Presets.Options;
+using MultiConverter.ViewModels.Presets.Options;
+
+namespace MultiConverter.ViewModelsFixtures.Presets;
+
+[TestFixture]
+public class AudioBitrateOptionViewModelTests
+{
+    [Test]
+    public void New_VideoFrameRateOptionViewModel_After_Initialization()
+    {
+        AudioBitrateOptionViewModel fixture = InitializeFixture();
+
+        fixture.Bitrate.Should().Be(DefaultBitrate);
+        fixture.HasChanged.Should().BeFalse();
+        fixture.DefaultOptions.Length.Should().Be(4);
+    }
+
+    [Test]
+    public void After_value_changed_HasChanged_should_be_true()
+    {
+        const int newBitrate = 96;
+        AudioBitrateOptionViewModel fixture = InitializeFixture();
+
+        fixture.Bitrate = newBitrate;
+
+        fixture.HasChanged.Should().BeTrue();
+        fixture.Bitrate.Should().Be(newBitrate);
+    }
+
+    [Test]
+    [TestCase(0, 128)]
+    [TestCase(1, 196)]
+    [TestCase(2, 256)]
+    [TestCase(3, 320)]
+    public void Executing_ValuesUpdater_should_update_value(int index, int bitrate)
+    {
+        AudioBitrateOptionViewModel fixture = InitializeFixture(0);
+        ValuesUpdater updater = fixture.DefaultOptions[index];
+
+        updater.Update.Invoke();
+
+        fixture.Bitrate.Should().Be(bitrate);
+        fixture.HasChanged.Should().BeTrue();
+    }
+
+    [Test]
+    public void After_set_initial_value_HasChanged_should_be_false()
+    {
+        AudioBitrateOptionViewModel fixture = InitializeFixture();
+
+        fixture.Bitrate = 32;
+        fixture.Bitrate = DefaultBitrate;
+
+        fixture.HasChanged.Should().BeFalse();
+        fixture.Bitrate.Should().Be(DefaultBitrate);
+    }
+
+
+    private static AudioBitrateOptionViewModel InitializeFixture(int? initialBitrate = null)
+    {
+        ISchedulerProvider scheduler = new ImmediateSchedulers();
+        AudioBitrateOption option = new(initialBitrate ?? DefaultBitrate);
+        return new AudioBitrateOptionViewModel(option, scheduler);
+    }
+
+    private const int DefaultBitrate = 128;
+}
+
