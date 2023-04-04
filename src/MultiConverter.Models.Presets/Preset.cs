825a0cdfe1f195214ae9c02912ece8427ab06817
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-04-04 13:06:03 -0400

    Rename Preset IsDefault property

--- src/MultiConverter.Models.Presets/Preset.cs
+++ src/MultiConverter.Models.Presets/Preset.cs
@@ -2,4 +2,4 @@
 
 namespace MultiConverter.Models.Presets;
 
-public record Preset(string Name, bool Default, VideoFilter[] VideoFilter, AudioFilter[] AudioFilter, IOption[] Options);
+public record Preset(string Name, bool IsDefault, VideoFilter[] VideoFilter, AudioFilter[] AudioFilter, IOption[] Options);
