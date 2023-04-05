dee364a2c55aad724eea90a42fbe55b1498e8506
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-04-05 19:30:04 -0400

    Set VideoFrameRateOption default parameters

--- src/MultiConverter.Models.Presets/Options/VideoFrameRateOption.cs
+++ src/MultiConverter.Models.Presets/Options/VideoFrameRateOption.cs
@@ -3,7 +3,7 @@ using MultiConverter.Models.Presets.Base;
 
 namespace MultiConverter.Models.Presets.Options;
 
-public record VideoFrameRateOption(double FrameRate) : OptionBase
+public record VideoFrameRateOption(double FrameRate = 29.97) : OptionBase
 {
     public override IArgument GetArgument() => new FrameRateArgument(FrameRate);
 }
