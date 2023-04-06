89ac038697e2cd7fa5a84dc2a7d312a99685a4d6
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-04-06 19:52:23 -0400

    Fix missing attribute

--- tests/MultiConverter.ViewModelsFixtures/Presets/Options/Providers/OptionGeneratorStrategyTests.cs
+++ tests/MultiConverter.ViewModelsFixtures/Presets/Options/Providers/OptionGeneratorStrategyTests.cs
@@ -6,6 +6,7 @@ using MultiConverter.ViewModelsFixtures.Helper;
 
 namespace MultiConverter.ViewModelsFixtures.Presets.Options.Providers;
 
+[TestFixture]
 public class OptionGeneratorStrategyTests
 {
     [Test]
