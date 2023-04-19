661f4b254105707eef0608254115029c72819e92
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-04-19 14:20:53 -0400

    Cleanup Preset class

--- src/MultiConverter.Models.Presets/Preset.cs
+++ src/MultiConverter.Models.Presets/Preset.cs
@@ -5,7 +5,7 @@ namespace MultiConverter.Models.Presets;
 public record Preset(string Name, bool IsDefault, VideoFilter[] VideoFilter, AudioFilter[] AudioFilter,
     IOption[] Options)
 {
-    public static Preset Empty => new Preset(
+    public static Preset Empty => new(
         string.Empty,
         false,
         Array.Empty<VideoFilter>(),
