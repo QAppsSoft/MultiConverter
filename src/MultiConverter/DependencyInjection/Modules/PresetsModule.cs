8acb3d89e3989957bd0e94d0e95f82fa4ade71d0
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-04-22 04:47:07 -0400

    DI - Register Presets in his own module

---
+++ src/MultiConverter/DependencyInjection/Modules/PresetsModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using MultiConverter.Models.Presets;
+using MultiConverter.Services.Abstractions.Settings;
+using MultiConverter.Services.Settings.Presets;
+
+namespace MultiConverter.DependencyInjection.Modules;
+
+public class PresetsModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        RegisterConverters(builder);
+        RegisterPresets(builder);
+    }
+
+    private static void RegisterPresets(ContainerBuilder builder) =>
+        builder.Register(context =>
+        {
+            var factory = context.Resolve<ISettingFactory>();
+            IConverter<Preset[]> converter = context.Resolve<IConverter<Preset[]>>();
+            return factory.Create(converter, "Presets");
+        }).SingleInstance();
+
+    private static void RegisterConverters(ContainerBuilder builder)
+    {
+        builder.RegisterType<PresetsConverter>()
+            .As<IConverter<Preset[]>>()
+            .SingleInstance();
+    }
+}
--- src/MultiConverter/DependencyInjection/Modules/SettingsModule.cs
+++ src/MultiConverter/DependencyInjection/Modules/SettingsModule.cs
@@ -1,10 +1,8 @@
 ﻿using Autofac;
-using MultiConverter.Models.Presets;
 using MultiConverter.Models.Settings.General;
 using MultiConverter.Services.Abstractions.Settings;
 using MultiConverter.Services.Settings;
 using MultiConverter.Services.Settings.General;
-using MultiConverter.Services.Settings.Presets;
 
 namespace MultiConverter.DependencyInjection.Modules;
 
@@ -15,7 +13,6 @@ public class SettingsModule : Module
         RegisterCommonInterfaces(builder);
         RegisterConverters(builder);
         RegisterSettings(builder);
-        RegisterPresets(builder);
     }
 
     private static void RegisterSettings(ContainerBuilder builder) =>
@@ -26,22 +23,10 @@ public class SettingsModule : Module
             return factory.Create(converter, "GeneralOptions");
         }).SingleInstance();
 
-    private static void RegisterPresets(ContainerBuilder builder) =>
-        builder.Register(context =>
-        {
-            var factory = context.Resolve<ISettingFactory>();
-            IConverter<Preset[]> converter = context.Resolve<IConverter<Preset[]>>();
-            return factory.Create(converter, "Presets");
-        }).SingleInstance();
-
     private static void RegisterConverters(ContainerBuilder builder)
     {
         builder.RegisterType<GeneralOptionsConverter>()
             .As<IConverter<GeneralOptions>>()
-            .SingleInstance();
-
-        builder.RegisterType<PresetsConverter>()
-            .As<IConverter<Preset[]>>()
             .SingleInstance();
     }
 
