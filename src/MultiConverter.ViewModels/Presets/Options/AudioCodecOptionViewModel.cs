36ed8a2f37c5427dad89a6f4ef4e8f96c45be833
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-14 19:34:58 -0400

    Re-Implement Audio Codec selection

--- src/MultiConverter.ViewModels/Presets/Options/AudioCodecOptionViewModel.cs
+++ src/MultiConverter.ViewModels/Presets/Options/AudioCodecOptionViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Reactive.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
 using MultiConverter.Common;
+using MultiConverter.Models.Presets.Formats;
 using MultiConverter.Models.Presets.Interfaces;
 using MultiConverter.Models.Presets.Options;
+using MultiConverter.Services.Abstractions.Formats;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
@@ -9,31 +13,47 @@ namespace MultiConverter.ViewModels.Presets.Options;
 
 public sealed class AudioCodecOptionViewModel : OptionViewModelBase
 {
-    public AudioCodecOptionViewModel(AudioCodecOption audioCodecOption, ISchedulerProvider schedulerProvider) : base(schedulerProvider)
-    {
-        AudioCodec = audioCodecOption.AudioCodec;
+    private readonly string _codec;
 
-        _ = this.WhenAnyValue(x => x.AudioCodec)
-            .Select(codec => codec != audioCodecOption.AudioCodec)
+    public AudioCodecOptionViewModel(AudioCodecOption audioCodecOption, ICodecsProvider codecsProvider, ISchedulerProvider schedulerProvider) : base(schedulerProvider)
+    {
+        _codec = audioCodecOption.AudioCodec;
+        Codecs = codecsProvider.GetAudioCodecs();
+
+        InitializeSelectedCodec();
+        InitializeDefaultOptions();
+
+        _ = this.WhenAnyValue(x => x.SelectedCodec)
+            .Select(codec => codec.Name != audioCodecOption.AudioCodec)
             .ObserveOn(schedulerProvider.Dispatcher)
             .ToPropertyEx(this, x => x.HasChanged);
-
-        InitializeDefaultOptions();
     }
 
-    [Reactive] public string AudioCodec { get; set; }
+    public IEnumerable<Codec> Codecs { get; }
+
+    [Reactive] public Codec SelectedCodec { get; set; }
+
+    private void InitializeSelectedCodec()
+    {
+        SelectedCodec = SelectCodec(_codec);
+    }
+
+    private Codec SelectCodec(string codecName)
+    {
+        return Codecs.First(codec => codec.Name == codecName);
+    }
 
     private void InitializeDefaultOptions() =>
         DefaultOptions = new[]
         {
-            new ValuesUpdater { Caption = "MP2", Update = () => AudioCodec = "mp2" },
-            new ValuesUpdater { Caption = "MP3", Update = () => AudioCodec = "libmp3lame" },
-            new ValuesUpdater { Caption = "AAC", Update = () => AudioCodec = "aac" }
+            new ValuesUpdater { Caption = "MP2", Update = () => SelectedCodec = SelectCodec("mp2") },
+            new ValuesUpdater { Caption = "MP3", Update = () => SelectedCodec = SelectCodec("libmp3lame") },
+            new ValuesUpdater { Caption = "AAC", Update = () => SelectedCodec = SelectCodec("aac") }
         };
 
-    public static implicit operator AudioCodecOption(AudioCodecOptionViewModel vm) => new(vm.AudioCodec);
+    public static implicit operator AudioCodecOption(AudioCodecOptionViewModel vm) => new(vm.SelectedCodec.Name);
 
-    private static IOption ToOption(AudioCodecOptionViewModel vm) => new AudioCodecOption(vm.AudioCodec);
+    private static IOption ToOption(AudioCodecOptionViewModel vm) => new AudioCodecOption(vm.SelectedCodec.Name);
 
     public override IOption GetOption() => ToOption(this);
 }
