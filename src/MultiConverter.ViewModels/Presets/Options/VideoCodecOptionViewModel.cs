be5cb5228d078e52ed0df051a199d70bded20bb0
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-14 19:15:33 -0400

    Re-Implement Video Codec selection

--- src/MultiConverter.ViewModels/Presets/Options/VideoCodecOptionViewModel.cs
+++ src/MultiConverter.ViewModels/Presets/Options/VideoCodecOptionViewModel.cs
@@ -1,43 +1,62 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Linq;
 using DynamicData.Binding;
 using MultiConverter.Common;
 using MultiConverter.Models.Presets.Interfaces;
 using MultiConverter.Models.Presets.Options;
+using MultiConverter.Services.Abstractions.Formats;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using Codec = MultiConverter.Models.Presets.Formats.Codec;
 
 namespace MultiConverter.ViewModels.Presets.Options;
 
 public sealed class VideoCodecOptionViewModel : OptionViewModelBase
 {
-    public VideoCodecOptionViewModel(VideoCodecOption videoCodec, ISchedulerProvider schedulerProvider) :
+    private readonly string _codec;
+
+    public VideoCodecOptionViewModel(VideoCodecOption videoCodec, ICodecsProvider codecsProvider, ISchedulerProvider schedulerProvider) :
         base(schedulerProvider)
     {
-        Codec = videoCodec.Codec;
+        _codec = videoCodec.Codec;
+        Codecs = codecsProvider.GetVideoCodecs();
+
+        InitializeSelectedCodec();
+        InitializeDefaultOptions();
 
         _ = this.WhenAnyPropertyChanged()
             .WhereNotNull()
-            .Select(x => videoCodec.Codec != x.Codec)
+            .Select(vm => videoCodec.Codec != vm.SelectedCodec.Name)
             .ObserveOn(schedulerProvider.Dispatcher)
-            .ToPropertyEx(this, x => x.HasChanged);
-
-        InitializeDefaultOptions();
+            .ToPropertyEx(this, vm => vm.HasChanged);
     }
 
-    [Reactive] public string Codec { get; set; }
+    public IEnumerable<Codec> Codecs { get; }
+
+    [Reactive] public Codec SelectedCodec { get; set; }
+
+    private void InitializeSelectedCodec()
+    {
+        SelectedCodec = SelectCodec(_codec);
+    }
+
+    private Codec SelectCodec(string codecName)
+    {
+        return Codecs.First(codec => codec.Name == codecName);
+    }
 
     private void InitializeDefaultOptions() => DefaultOptions = new[]
     {
-        new ValuesUpdater { Caption = "MPG1", Update = () => Codec = "mpeg1video" },
-        new ValuesUpdater { Caption = "MPG2", Update = () => Codec = "mpeg2video" },
-        new ValuesUpdater { Caption = "MP4", Update = () => Codec = "mpeg4" },
-        new ValuesUpdater { Caption = "AVI", Update = () => Codec = "libxvid" },
+        new ValuesUpdater { Caption = "MPEG-1", Update = () => SelectedCodec = SelectCodec("mpeg1video") },
+        new ValuesUpdater { Caption = "MPEG-2", Update = () => SelectedCodec = SelectCodec("mpeg2video") },
+        new ValuesUpdater { Caption = "MPEG-4", Update = () => SelectedCodec = SelectCodec("mpeg4") },
+        new ValuesUpdater { Caption = "AVI (Xvid)", Update = () => SelectedCodec = SelectCodec("libxvid") },
     };
 
-    public static implicit operator VideoCodecOption(VideoCodecOptionViewModel vm) => new(vm.Codec);
+    public static implicit operator VideoCodecOption(VideoCodecOptionViewModel vm) => new(vm.SelectedCodec.Name);
 
-    private static IOption ToOption(VideoCodecOptionViewModel vm) => new VideoCodecOption(vm.Codec);
+    private static IOption ToOption(VideoCodecOptionViewModel vm) => new VideoCodecOption(vm.SelectedCodec.Name);
 
     public override IOption GetOption() => ToOption(this);
 }
