89b2a6a4ba165b32e074c69fd99ca7511da51a40
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-14 14:01:33 -0400

    Implement codec and container models

---
+++ src/MultiConverter.Models.Presets/Enums/CodecType.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiConverter.Models.Presets.Enums;
+
+public enum CodecType
+{
+    Unknown = 0,
+    Video = 2,
+    Audio = 4,
+    Subtitle = 8,
+    Data = 16
+}
---
+++ src/MultiConverter.Models.Presets/Formats/MediaCodec.cs
@@ -0,0 +1,5 @@
+﻿using MultiConverter.Models.Presets.Enums;
+
+namespace MultiConverter.Models.Presets.Formats;
+
+public record MediaCodec(string Name, CodecType Type, bool DecodingSupported, bool EncodingSupported, string Description);
