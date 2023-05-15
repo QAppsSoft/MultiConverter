5d8d43316578b7f5fe80fcb2d4be807623ae945e
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-15 18:22:15 -0400

    Include Favorite property in ContainerFormat

--- src/MultiConverter.Services.Formats/ContainersFormatProvider.cs
+++ src/MultiConverter.Services.Formats/ContainersFormatProvider.cs
@@ -1,4 +1,5 @@
 ﻿using FFMpegCore;
+using MultiConverter.Models.Configurations;
 using MultiConverter.Models.Presets.Formats;
 using MultiConverter.Services.Abstractions.Formats;
 
@@ -6,24 +7,34 @@ namespace MultiConverter.Services.Formats;
 
 public class ContainersFormatProvider : IContainersFormatProvider
 {
+    private readonly FavoriteFormatsConfiguration _configuration;
     private readonly Lazy<IEnumerable<ContainerFormat>> _containers;
 
-    public ContainersFormatProvider()
+    public ContainersFormatProvider(FavoriteFormatsConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
+        _configuration = configuration;
+
         _containers = new Lazy<IEnumerable<ContainerFormat>>(ContainersFactory);
     }
 
     public IEnumerable<ContainerFormat> Formats() => _containers.Value;
 
-    private static IEnumerable<ContainerFormat> ContainersFactory()
+    private IEnumerable<ContainerFormat> ContainersFactory()
     {
         var containers = FFMpeg.GetContainerFormats();
         return containers.Where(f => f.MuxingSupported)
             .Select(format =>
-                new ContainerFormat(
+            {
+                bool favorite = _configuration.Favorites.Contains(format.Name);
+                return new ContainerFormat(
                     format.Name,
                     format.Description,
-                    format.Extension))
+                    format.Extension,
+                    favorite
+                );
+            })
             .OrderBy(format => format.Extension);
     }
 }
