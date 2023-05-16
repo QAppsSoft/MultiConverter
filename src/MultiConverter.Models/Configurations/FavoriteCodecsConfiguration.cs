0682c3f24de14466a2051309e816743a63651049
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-15 21:29:47 -0400

    Implement and register FavoriteCodecsConfiguration

---
+++ src/MultiConverter.Models/Configurations/FavoriteCodecsConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace MultiConverter.Models.Configurations;
+
+public sealed class FavoriteCodecsConfiguration
+{
+    public List<string> Favorites { get; init; } = null!;
+}
