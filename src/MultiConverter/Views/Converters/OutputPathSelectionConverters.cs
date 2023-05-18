c86b6a8b074be73f8490680b3e5cbc52e838e1ac
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-18 16:48:25 -0400

    Update UI changes

---
+++ src/MultiConverter/Views/Converters/OutputPathSelectionConverters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using MultiConverter.Models.Presets.Enums;
+
+namespace MultiConverter.Views.Converters;
+
+public static class OutputPathSelectionConverter
+{
+    public static IValueConverter IsVisibleIf => new OutputPathSelectionToBoolConverter();
+}
+
+public class OutputPathSelectionToBoolConverter : ConvertBase
+{
+    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        switch (value)
+        {
+            case null:
+                return null;
+            case OutputPathSelection pathSelection when parameter is OutputPathSelection parameterValue:
+                return pathSelection == parameterValue;
+            default:
+                {
+                    string message = $"Could not convert '{value}' to '{targetType.Name}' with parameter '{parameter}'.";
+                    return new BindingNotification(new InvalidCastException(message), BindingErrorType.Error);
+                }
+        }
+    }
+}
