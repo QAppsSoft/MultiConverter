3e574dff149748bc271091fbb5144c825aed41fe
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-18 16:47:35 -0400

    Update ViewModel with new changes

--- src/MultiConverter.ViewModels/Presets/Factories/OutputTemplateViewModelFactory.cs
+++ src/MultiConverter.ViewModels/Presets/Factories/OutputTemplateViewModelFactory.cs
@@ -1,6 +1,7 @@
 using System;
 using MultiConverter.Common;
 using MultiConverter.Models.Presets.Output;
+using MultiConverter.Services;
 using MultiConverter.ViewModels.Presets.Interfaces;
 using MultiConverter.ViewModels.Presets.Output;
 
@@ -9,13 +10,16 @@ namespace MultiConverter.ViewModels.Presets.Factories;
 public sealed class OutputTemplateViewModelFactory : IOutputTemplateViewModelFactory
 {
     private readonly ISchedulerProvider _schedulerProvider;
+    private readonly IDialogService _dialogService;
 
-    public OutputTemplateViewModelFactory(ISchedulerProvider schedulerProvider)
+    public OutputTemplateViewModelFactory(ISchedulerProvider schedulerProvider, IDialogService dialogService)
     {
         ArgumentNullException.ThrowIfNull(schedulerProvider);
+        ArgumentNullException.ThrowIfNull(dialogService);
         _schedulerProvider = schedulerProvider;
+        _dialogService = dialogService;
     }
 
     public OutputTemplateViewModel Build(OutputPathTemplate outputPathTemplate) =>
-        new(outputPathTemplate, _schedulerProvider);
+        new(outputPathTemplate, _schedulerProvider, _dialogService);
 }
--- src/MultiConverter.ViewModels/Presets/Output/OutputTemplateViewModel.cs
+++ src/MultiConverter.ViewModels/Presets/Output/OutputTemplateViewModel.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using MultiConverter.Common;
 using MultiConverter.Extensions;
+using MultiConverter.Models.Presets.Enums;
 using MultiConverter.Models.Presets.Output;
+using MultiConverter.Services;
 using MultiConverter.ViewModels.Presets.Interfaces;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -14,15 +18,23 @@ public sealed class OutputTemplateViewModel : ViewModelBase, IChanged, IDisposab
 {
     private readonly CompositeDisposable _cleanup = new();
 
-    public OutputTemplateViewModel(OutputPathTemplate outputPathTemplate, ISchedulerProvider schedulerProvider)
+    public OutputTemplateViewModel(OutputPathTemplate outputPathTemplate, ISchedulerProvider schedulerProvider, IDialogService dialogService)
     {
         ArgumentNullException.ThrowIfNull(outputPathTemplate);
         ArgumentNullException.ThrowIfNull(schedulerProvider);
 
         InitializeProperties(outputPathTemplate);
 
+        SelectFolder = ReactiveCommand.CreateFromTask(() => UpdateOutputPath(dialogService));
+
         HasChangedObservable(outputPathTemplate).ToPropertyEx(this, vm => vm.HasChanged).DisposeWith(_cleanup);
     }
+
+    [Reactive] public OutputPathSelection OutputPathSelected { get; set; }
+
+    [Reactive] public string FixedPath { get; set; }
+
+    [Reactive] public string AddInPathCollision { get; set; }
 
     [Reactive] public string Template { get; set; }
 
@@ -32,23 +44,60 @@ public sealed class OutputTemplateViewModel : ViewModelBase, IChanged, IDisposab
 
     [ObservableAsProperty] public bool HasChanged { get; }
 
+    public OutputPathSelection[] OutputPathItems { get; } =
+    {
+        OutputPathSelection.SameAsInput, OutputPathSelection.FixedPath, OutputPathSelection.AdvancedTemplate
+    };
+
+    public ReactiveCommand<Unit, Unit> SelectFolder { get; }
+
+    private async Task UpdateOutputPath(IDialogService dialogService)
+    {
+        FolderDialogSettings dialogSetting = new(FixedPath);
+
+        string? fixedPath = await dialogService.ShowFolderSelectorAsync(dialogSetting);
+
+        if (fixedPath == null)
+        {
+            return;
+        }
+
+        FixedPath = fixedPath;
+    }
+
     private void InitializeProperties(OutputPathTemplate outputPathTemplate)
     {
+        OutputPathSelected = outputPathTemplate.OutputPathSelection;
         Template = outputPathTemplate.Template;
         OverrideContainerExtension = outputPathTemplate.OverrideContainerExtension;
         OutputExtension = outputPathTemplate.OutputExtension;
+        FixedPath = outputPathTemplate.FixedPath;
+        AddInPathCollision = outputPathTemplate.AddInPathCollision;
     }
 
     private IObservable<bool> HasChangedObservable(OutputPathTemplate outputPathTemplate) =>
         new[]
         {
+            this.WhenAnyValue(vm => vm.OutputPathSelected).Select(selection => selection != outputPathTemplate.OutputPathSelection),
             this.WhenAnyValue(vm => vm.Template).Select(template => template != outputPathTemplate.Template),
             this.WhenAnyValue(vm => vm.OverrideContainerExtension).Select(overrideExtension => overrideExtension != outputPathTemplate.OverrideContainerExtension),
-            this.WhenAnyValue(vm => vm.OutputExtension).Select(extension => extension != outputPathTemplate.OutputExtension)
+            this.WhenAnyValue(vm => vm.OutputExtension).Select(extension => extension != outputPathTemplate.OutputExtension),
+            this.WhenAnyValue(vm => vm.FixedPath).Select(fixedPath => fixedPath != outputPathTemplate.FixedPath),
+            this.WhenAnyValue(vm => vm.AddInPathCollision).Select(inCollision => inCollision != outputPathTemplate.AddInPathCollision),
         }.CombineLatest(values => values.AnyIsTrue());
 
-    public static implicit operator OutputPathTemplate(OutputTemplateViewModel vm) =>
-        new(vm.Template, vm.OverrideContainerExtension, vm.OutputExtension);
+    public static implicit operator OutputPathTemplate(OutputTemplateViewModel vm) => new(
+        vm.OutputPathSelected,
+        vm.Template,
+        vm.OverrideContainerExtension,
+        vm.OutputExtension,
+        vm.FixedPath,
+        vm.AddInPathCollision
+    );
 
-    public void Dispose() => _cleanup.Dispose();
+    public void Dispose()
+    {
+        SelectFolder.Dispose();
+        _cleanup.Dispose();
+    }
 }
