6a8ce3dfcb62622b9802908233c512a9160ad553
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-19 12:39:44 -0400

    Implement ExtensionOverridesConfiguration

---
+++ src/MultiConverter.Models/Configurations/ExtensionOverridesConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace MultiConverter.Models.Configurations;
+
+public sealed class ExtensionOverridesConfiguration
+{
+    public Dictionary<string, string> Overrides { get; init; } = null!;
+}
