7bad81215f772f930c6a94c7c0059a2671f5b39c
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-05-22 08:34:36 -0400

    Update PresetBuilder

--- src/MultiConverter.Models.Presets/Builder/PresetBuilder.cs
+++ src/MultiConverter.Models.Presets/Builder/PresetBuilder.cs
@@ -6,6 +6,10 @@ namespace MultiConverter.Models.Presets.Builder;
 
 public sealed class PresetBuilder
 {
+    private bool _isAdvanced;
+    private string _name;
+    private bool _defaultPreset;
+    private string _format;
     private List<OptionBase> Options { get; } = new();
     private List<AudioFilter> AudioFilters { get; } = new();
     private List<VideoFilter> VideoFilters { get; } = new();
@@ -55,9 +59,43 @@ public sealed class PresetBuilder
         return this;
     }
 
+    public PresetBuilder IsAdvanced(bool advanced = true)
+    {
+        _isAdvanced = advanced;
+        return this;
+    }
+
+    public PresetBuilder IsDefault(bool defaultPreset = true)
+    {
+        _defaultPreset = defaultPreset;
+        return this;
+    }
+
+    public PresetBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public PresetBuilder WithFormat(string format)
+    {
+        _format = format;
+        return this;
+    }
+
     public Preset Build()
     {
-        return new Preset(string.Empty, false, VideoFilters.ToArray(), AudioFilters.ToArray(), Options.ToArray(),
-            false, string.Empty, InputPostConversion.Default, SubtitleStyle.Default, OutputPathTemplate.Default);
+        return new Preset(
+            _name,
+            _defaultPreset,
+            VideoFilters.ToArray(),
+            AudioFilters.ToArray(),
+            Options.ToArray(),
+            _isAdvanced,
+            _format,
+            InputPostConversion.Default,
+            SubtitleStyle.Default,
+            OutputPathTemplate.Default
+        );
     }
 }
