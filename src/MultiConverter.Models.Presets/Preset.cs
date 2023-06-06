e128cd076a0be1909f06c2e00996bd278284d58c
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-06-06 16:50:26 -0400

    Implement Preset equality

--- src/MultiConverter.Models.Presets/Preset.cs
+++ src/MultiConverter.Models.Presets/Preset.cs
@@ -9,6 +9,46 @@ public record Preset(string Name, bool IsDefault, VideoFilter[] VideoFilter, Aud
     IOption[] Options, bool IsAdvanced, string ContainerFormat, InputPostConversion PostConversion,
     SubtitleStyle SubtitleStyle, OutputPathTemplate OutputPathTemplate)
 {
+    public virtual bool Equals(Preset? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Name == other.Name &&
+               IsDefault == other.IsDefault &&
+               VideoFilter.SequenceEqual(other.VideoFilter) &&
+               AudioFilter.SequenceEqual(other.AudioFilter) &&
+               Options.SequenceEqual(other.Options) &&
+               IsAdvanced == other.IsAdvanced &&
+               ContainerFormat == other.ContainerFormat &&
+               PostConversion.Equals(other.PostConversion) &&
+               SubtitleStyle.Equals(other.SubtitleStyle) &&
+               OutputPathTemplate.Equals(other.OutputPathTemplate);
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(Name);
+        hashCode.Add(IsDefault);
+        hashCode.Add(VideoFilter);
+        hashCode.Add(AudioFilter);
+        hashCode.Add(Options);
+        hashCode.Add(IsAdvanced);
+        hashCode.Add(ContainerFormat);
+        hashCode.Add(PostConversion);
+        hashCode.Add(SubtitleStyle);
+        hashCode.Add(OutputPathTemplate);
+        return hashCode.ToHashCode();
+    }
+
     private static IOption[] GetBasicOptions { get; } = {
         new VideoSizeOption(VideoSize.Default),
         new VideoCodecOption(VideoCodecOption.Default),
