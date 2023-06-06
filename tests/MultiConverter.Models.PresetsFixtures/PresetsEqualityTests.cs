1fab7d69ff3017e12c725890a478021c63a05fa0
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2023-06-06 16:52:45 -0400

    Test Preset equality implementation

---
+++ tests/MultiConverter.Models.PresetsFixtures/PresetsEqualityTests.cs
@@ -0,0 +1,79 @@
+﻿using MultiConverter.Models.Presets;
+using MultiConverter.Models.Presets.AudioFilters;
+using MultiConverter.Models.Presets.Interfaces;
+using MultiConverter.Models.Presets.Options;
+using MultiConverter.Models.Presets.VideoFilters;
+
+namespace MultiConverter.Models.PresetsFixtures;
+
+[TestFixture]
+public class PresetsEqualityTests
+{
+    [Test]
+    public void Compare_two_empty_presets_should_be_equal()
+    {
+        Preset p1 = EmptyPreset();
+        Preset p2 = EmptyPreset();
+
+        bool equals = p1.Equals(p2);
+
+        equals.Should().BeTrue();
+    }
+
+    [Test]
+    public void Compare_two_equals_non_empty_presets_should_be_equal()
+    {
+        Preset p1 = FilledPreset();
+        Preset p2 = FilledPreset();
+
+        bool equals = p1.Equals(p2);
+
+        equals.Should().BeTrue();
+    }
+
+    [Test]
+    public void Compare_two_array_of_empty_presets_should_be_equal()
+    {
+        Preset[] a1 = { EmptyPreset(), FilledPreset() };
+        Preset[] a2 = { EmptyPreset(), FilledPreset() };
+
+        bool equals = a1.SequenceEqual(a2);
+
+        equals.Should().BeTrue();
+    }
+
+    [Test]
+    public void Compare_two_array_of_different_presets_should_not_be_equal()
+    {
+        Preset[] a1 = { FilledPreset(), EmptyPreset() };
+        Preset[] a2 = { EmptyPreset(), FilledPreset() };
+
+        bool equals = a1.SequenceEqual(a2);
+
+        equals.Should().BeFalse();
+    }
+
+    [Test]
+    public void Compare_two_array_of_slightly_different_presets_should_not_be_equal()
+    {
+        Preset[] a1 =
+        {
+            EmptyPreset(),
+            EmptyPreset() with { VideoFilter = new VideoFilter[] { new VideoFilterScale(1024, 768) } }
+        };
+        Preset[] a2 = { EmptyPreset(), EmptyPreset() };
+
+        bool equals = a1.SequenceEqual(a2);
+
+        equals.Should().BeFalse();
+    }
+
+    private static Preset EmptyPreset() => Preset.Empty;
+
+    private static Preset FilledPreset() => Preset.Empty with
+    {
+        VideoFilter = new VideoFilter[] { new VideoFilterScale(1024, 768) },
+        AudioFilter = new AudioFilter[] { new AudioFilterDynamicNormalizer() },
+        Options = new IOption[] { new AudioCodecOption("mpeg4") }
+    };
+}
